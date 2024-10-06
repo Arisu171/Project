2a8d99b3083f7bd9d0306d0b65009f85bc51854f
BAO035800 <baomoren50@gmail.com>
2024-10-06 15:09:42 +0700

    1

--- General/Nav.js
+++ General/Nav.js
@@ -2,6 +2,73 @@ const navbut = document.getElementById('navicon');
 const button = document.getElementById('navbutt');
 const main = document.getElementById('main');
 
+// Đảm bảo sự kiện chỉ chạy khi trang đã được tải hoàn toàn
+window.onload = function() {
+    // Lấy phần tử có id là 'content'
+    var navigationbar = document.getElementById('navigationbar');
+    
+    // Chèn nội dung HTML vào phần tử
+    navigationbar.innerHTML = `
+    <div id="shadow"></div>
+    <div class="navbar">
+        <div class="navleft"><a href="#bannerimg1"></a></div>
+        <div class="navright" id="navright">
+            <div class="navtitle"><a href="#bannerimg1">Home</a></div>
+            <div class="navdrop">
+                <div class="navtitle">Pages
+                    <div class="navdroplist">
+                        <!-- <div class="navli">About Us</div>
+                        <div class="navli">Contact Us</div> -->
+                    </div>
+                </div>
+            </div>
+            <div class="navtitle">Tourist</div>
+            <div class="navtitle">Regions</div>
+            <div class="navdrop">
+                <div class="navtitle">Transportations
+                    <div class="navdroplist">
+                        <!-- <div class="navli">Airport Transport</div>
+                        <div class="navli">Bus and Shuttle</div>
+                        <div class="navli">Car Rental</div> -->
+                    </div>
+                </div>
+            </div>
+            <div class="navtitle">Blog</div>
+            <div class="navuser"><i class="fa fa-user"></i></div>
+        </div>
+        <div class="navbutton navradius" id="navbutt">
+            <div class="navicon" id="navicon"><i class="fa-solid fa-bars"></i></div>
+            <div class="navitem">
+                <a href="#bannerimg1">
+                    <i class="fa-solid fa-house"></i>
+                    <div class="navitcap">Home</div>
+                </a>
+            </div>
+            <div class="navitem">
+                <i class="fa-solid fa-users"></i>
+                <div class="navitcap">Pages</div>
+            </div>
+            <div class="navitem">
+                <i class="fa-solid fa-calendar-days"></i>
+                <div class="navitcap">Tourist</div>
+            </div>
+            <div class="navitem">
+                <i class="fa-solid fa-globe"></i>
+                <div class="navitcap">Regions</div>
+            </div>
+            <div class="navitem">
+                <i class="fa-solid fa-bus"></i>
+                <div class="navitcap">Transpos</div>
+            </div>
+            <div class="navitem">
+                <i class="fa-brands fa-squarespace"></i>
+                <div class="navitcap">Blog</div>
+            </div>
+            <div class="navuser"><i class="fa fa-user"></i></div>
+        </div>
+    </div>
+    `;
+};
 // Thêm class khi click chuột
 navbut.addEventListener('click', function () {
     if (button.classList.contains('navcol')) {
