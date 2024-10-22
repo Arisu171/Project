e41a7dd9fbf1e2b22e18a9b65315f3714ac2565a
BAO035800 <baomoren50@gmail.com>
2024-10-22 09:49:56 +0700

    12345

---
+++ Home/Region/4-MIỀN.js
@@ -0,0 +1,13 @@
+/*slide ảnh region */
+const images = document.querySelectorAll(".img-show img");
+function move() {
+    let tempSrc = images[0].src;
+    let tempAlt = images[0].alt;
+    let i;
+    for (i = 0; i < images.length - 1; i++) {
+        images[i].src = images[i + 1].src;
+        images[i].alt = images[i + 1].alt;
+    }
+    images[images.length - 1].src = tempSrc;
+    images[images.length - 1].alt = tempAlt;
+}
