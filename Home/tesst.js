83c566e4a3beab0d3a47f0cbe27e8fbef905f59a
Arisu171 <long171.d.shine@gmail.com>
2024-10-23 11:40:16 +0700

    big upd

--- Home/tesst.js
+++ Home/tesst.js
@@ -1,222 +1,236 @@
-let dragging = false;
-
-function makeElementDraggable(el) {
-    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-
-    // Sự kiện chuột cho máy tính
-    el.onmousedown = dragMouseDown;
-
-    // Sự kiện cảm ứng cho thiết bị di động
-    el.ontouchstart = dragTouchStart;
-
-    function dragMouseDown(e) {
-        e.preventDefault();
-        startDrag(e.clientX, e.clientY);
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
-    }
-
-    function dragTouchStart(e) {
-        const touch = e.touches[0];
-        startDrag(touch.clientX, touch.clientY);
-        document.ontouchend = closeDragElement;
-        document.ontouchmove = elementTouchDrag;
-    }
-
-    function startDrag(clientX, clientY) {
-        dragging = false;
-        pos3 = clientX;
-        pos4 = clientY;
-    }
-
-    function elementDrag(e) {
-        e.preventDefault();
-        dragAction(e.clientX, e.clientY);
-    }
-
-    function elementTouchDrag(e) {
-        const touch = e.touches[0];
-        dragAction(touch.clientX, touch.clientY);
-    }
-
-    function dragAction(clientX, clientY) {
-        dragging = true;
-        pos1 = pos3 - clientX;
-        pos2 = pos4 - clientY;
-        pos3 = clientX;
-        pos4 = clientY;
-        el.style.top = (el.offsetTop - pos2) + "px";
-        el.style.left = (el.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement() {
-        document.onmouseup = null;
-        document.onmousemove = null;
-        document.ontouchend = null;
-        document.ontouchmove = null;
-
-        const currentY = el.offsetTop;
-        const windowHeight = window.innerHeight;
-        let newY = currentY;
-
-        if (currentY < 1) {
-            newY = 1;
-        } else if (currentY > (windowHeight - el.offsetHeight - 1)) {
-            newY = windowHeight - el.offsetHeight - 1;
-        }
-
-        const windowWidth = window.innerWidth;
-        const currentX = el.offsetLeft;
-        let newX;
-
-        if (currentX < (windowWidth / 2)) {
-            newX = 1;
-
-            const rightMulticons = document.querySelectorAll('[id^="rmulticon"]');
-            rightMulticons.forEach(function (element) {
-                const newId = element.id.replace('rmulticon', 'lmulticon');
-                element.id = newId;
-            });
-        } else {
-            newX = windowWidth - el.offsetWidth;
-
-            const leftMulticons = document.querySelectorAll('[id^="lmulticon"]');
-            leftMulticons.forEach(function (element) {
-                const newId = element.id.replace('lmulticon', 'rmulticon');
-                element.id = newId;
-            });
-        }
-
-        setTimeout(() => {
-            el.style.left = newX + "px";
-            el.style.top = newY + "px";
-            dragging = false;
-        }, 100);
-    }
-}
-
-
-function shownav() {
-    if (!dragging) {
-        const elements = document.querySelectorAll('.navhide');
-        elements.forEach(function (element) {
-            if (element.classList.contains('show')) {
-                element.classList.remove('show');
-                element.classList.add('hide');
-                element.classList.add('over');
-            } else {
-                element.classList.add('show');
-                element.classList.remove('hide');
-                element.classList.add('disabled');
-                setTimeout(function () {
-                    element.classList.remove('over');
-                    element.classList.remove('disabled');
-                }, 500);
-            }
-        });
-    }
-}
-
-function checkScreenSize() {
-    const elements = document.querySelectorAll('.navhide');
-    if (window.innerWidth > 665) {
-        elements.forEach(function (element) {
-            element.classList.remove('show');
-            element.classList.add('hide');
-            element.classList.add('over');
-        });
-    }
-}
-
-function observeClassChanges(targetElement, classToRemove, affectedElementsSelector, classToRemoveFromAffected, classToAddFromAffected) {
-    const observer = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-            if (mutation.attributeName === "class") {
-                const currentClassList = mutation.target.classList;
-                if (!currentClassList.contains(classToRemove)) {
-                    const affectedElements = document.querySelectorAll(affectedElementsSelector);
-                    affectedElements.forEach(function (affectedElement) {
-                        affectedElement.classList.remove(classToRemoveFromAffected);
-                        affectedElement.classList.add(classToAddFromAffected);
-                    });
-                }
-            }
-        });
-    });
-
-    observer.observe(targetElement, { attributes: true });
-}
 window.onload = function () {
-    // Giả sử `isLoggedIn` là biến kiểm tra người dùng đã đăng nhập
-    let isLoggedIn = false; // Ban đầu, trạng thái là chưa đăng nhập
+    // Trạng thái đăng nhập
+    let isLoggedIn = false;
 
-    // Make nav button draggable
-    makeElementDraggable(document.getElementById("navbutt"));
-
-    // Attach resize event listener
-    window.addEventListener('resize', checkScreenSize);
-
-    // Monitor changes in class
-    const elementToWatch = document.querySelector('.navradius');
-    observeClassChanges(elementToWatch, 'show', '.multicon', 'show', 'hide');
-
-    // Event listeners for show/hide multicon lists
-    document.querySelector('#Pages div i').parentElement.addEventListener('click', function () {
-        const multicons = document.querySelectorAll('.multiconpage');
-        multicons.forEach(function (multiconpage) {
-            if (multiconpage.classList.contains('show')) {
-                multiconpage.classList.remove('show');
-                multiconpage.classList.add('hide');
-            } else {
-                multiconpage.classList.add('show');
-                multiconpage.classList.remove('hide');
-            }
-        });
-    });
-
-    document.querySelector('#Transpos div i').parentElement.addEventListener('click', function () {
-        const multicons = document.querySelectorAll('.multicontran');
-        multicons.forEach(function (multicontran) {
-            if (multicontran.classList.contains('show')) {
-                multicontran.classList.remove('show');
-                multicontran.classList.add('hide');
-            } else {
-                multicontran.classList.add('show');
-                multicontran.classList.remove('hide');
-            }
-        });
-    });
-
-    // **New Code for navuser multicon with login condition**
-    const navUserIcons = document.querySelectorAll('.navuser');
-    navUserIcons.forEach(function (navUserIcon) {
-        navUserIcon.addEventListener('click', function () {
-            if (!isLoggedIn) {
-                // Nếu người dùng đã đăng nhập, hiển thị multiconacc (Account và Log Out)
-                const multicons = document.querySelectorAll('.multiconacc');
-                multicons.forEach(function (multiconacc) {
-                    if (multiconacc.classList.contains('show')) {
-                        multiconacc.classList.remove('show');
-                        multiconacc.classList.add('hide');
-                    } else {
-                        multiconacc.classList.add('show');
-                        multiconacc.classList.remove('hide');
-                    }
-                });
-            } else {
-                // Nếu chưa đăng nhập, hiển thị phần tử login
-                const loginElement = document.getElementById('login');
-                if (loginElement) {
-                    loginElement.classList.add('show');
-                    loginElement.classList.remove('hide');
-                }
-            }
-        });
-    });
+    // Gắn các sự kiện sau khi DOM đã được cập nhật
+    attachEventHandlers();
 
     // **New Code: Login Button Click Event**
     const loginButton = document.getElementById('loginbutton');
-    loginButton.addEventListener('click', function () {
+    if (loginButton) {
+        loginButton.addEventListener('click', handleLogin);
+    }
+
+    // **New Code: Set isLoggedIn to false when clicking on elements with class "hide"**
+    const hideElements = document.querySelectorAll('.out');
+    hideElements.forEach(function (element) {
+        element.addEventListener('click', function () {
+            isLoggedIn = false;
+            toggleMulticons('.multiconacc');
+        });
+    });
+
+    function attachEventHandlers() {
+        // Make navigation button draggable
+        const navButt = document.getElementById("navbutt");
+        if (navButt) {
+            makeElementDraggable(navButt);
+        }
+
+        // Attach resize event listener
+        window.addEventListener('resize', checkScreenSize);
+
+        // Monitor changes in class
+        const elementToWatch = document.querySelector('.navradius');
+        if (elementToWatch) {
+            observeClassChanges(elementToWatch, 'show', '.multicon', 'show', 'hide');
+        }
+
+        // Event listeners for multicon lists
+        const pagesIcon = document.querySelector('#Pages div i');
+        if (pagesIcon) {
+            pagesIcon.parentElement.addEventListener('click', toggleMulticons.bind(null, '.multiconpage'));
+        }
+
+        const transposIcon = document.querySelector('#Transpos div i');
+        if (transposIcon) {
+            transposIcon.parentElement.addEventListener('click', toggleMulticons.bind(null, '.multicontran'));
+        }
+
+        // Add click listener to the navigation button
+        const navButton = document.getElementById("navicon");
+        if (navButton) {
+            navButton.addEventListener("click", shownav);
+        }
+
+        // Event listeners for nav user account
+        const navUserIcons = document.querySelectorAll('.navuser');
+        navUserIcons.forEach(function (navUserIcon) {
+            navUserIcon.addEventListener('click', function () {
+                if (isLoggedIn) {
+                    toggleMulticons('.multiconacc');
+                } else {
+                    const loginElement = document.getElementById('login');
+                    if (loginElement) {
+                        loginElement.classList.add('show');
+                        loginElement.classList.remove('hide');
+                    }
+                }
+            });
+        });
+    }
+
+    function toggleMulticons(selector) {
+        const multicons = document.querySelectorAll(selector);
+        multicons.forEach(function (multicon) {
+            if (multicon.classList.contains('show')) {
+                multicon.classList.remove('show');
+                multicon.classList.add('hide');
+            } else {
+                multicon.classList.add('show');
+                multicon.classList.remove('hide');
+            }
+        });
+    }
+
+    function checkScreenSize() {
+        const elements = document.querySelectorAll('.navhide');
+        if (window.innerWidth > 665) {
+            elements.forEach(function (element) {
+                element.classList.remove('show');
+                element.classList.add('hide');
+                element.classList.add('over');
+            });
+        }
+    }
+
+    function shownav() {
+        if (!dragging) {
+            const elements = document.querySelectorAll('.navhide');
+            elements.forEach(function (element) {
+                if (element.classList.contains('show')) {
+                    element.classList.remove('show');
+                    element.classList.add('hide');
+                    element.classList.add('over');
+                } else {
+                    element.classList.add('show');
+                    element.classList.remove('hide');
+                    element.classList.add('disabled');
+                    setTimeout(function () {
+                        element.classList.remove('over');
+                        element.classList.remove('disabled');
+                    }, 500);
+                }
+            });
+        }
+    }
+
+    function observeClassChanges(targetElement, classToRemove, affectedElementsSelector, classToRemoveFromAffected, classToAddFromAffected) {
+        const observer = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                if (mutation.attributeName === "class") {
+                    const currentClassList = mutation.target.classList;
+                    if (!currentClassList.contains(classToRemove)) {
+                        const affectedElements = document.querySelectorAll(affectedElementsSelector);
+                        affectedElements.forEach(function (affectedElement) {
+                            affectedElement.classList.remove(classToRemoveFromAffected);
+                            affectedElement.classList.add(classToAddFromAffected);
+                        });
+                    }
+                }
+            });
+        });
+
+        observer.observe(targetElement, { attributes: true });
+    }
+
+    // Dragging functionality
+    let dragging = false;
+
+    function makeElementDraggable(el) {
+        let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+        el.onmousedown = dragMouseDown;
+        el.ontouchstart = dragTouchStart;
+
+        function dragMouseDown(e) {
+            e.preventDefault();
+            startDrag(e.clientX, e.clientY);
+            document.onmouseup = closeDragElement;
+            document.onmousemove = elementDrag;
+        }
+
+        function dragTouchStart(e) {
+            const touch = e.touches[0];
+            startDrag(touch.clientX, touch.clientY);
+            document.ontouchend = closeDragElement;
+            document.ontouchmove = elementTouchDrag;
+        }
+
+        function startDrag(clientX, clientY) {
+            dragging = false;
+            pos3 = clientX;
+            pos4 = clientY;
+        }
+
+        function elementDrag(e) {
+            e.preventDefault();
+            dragAction(e.clientX, e.clientY);
+        }
+
+        function elementTouchDrag(e) {
+            const touch = e.touches[0];
+            dragAction(touch.clientX, touch.clientY);
+        }
+
+        function dragAction(clientX, clientY) {
+            dragging = true;
+            pos1 = pos3 - clientX;
+            pos2 = pos4 - clientY;
+            pos3 = clientX;
+            pos4 = clientY;
+            el.style.top = (el.offsetTop - pos2) + "px";
+            el.style.left = (el.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragElement() {
+            document.onmouseup = null;
+            document.onmousemove = null;
+            document.ontouchend = null;
+            document.ontouchmove = null;
+
+            const currentY = el.offsetTop;
+            const windowHeight = window.innerHeight;
+            let newY = currentY;
+
+            if (currentY < 7) {
+                newY = 7;
+            } else if (currentY > (windowHeight - el.offsetHeight - 7)) {
+                newY = windowHeight - el.offsetHeight - 7;
+            }
+
+            const windowWidth = window.innerWidth;
+            const currentX = el.offsetLeft;
+            let newX;
+
+            if (currentX < (windowWidth / 2)) {
+                newX = 7;
+
+                const rightMulticons = document.querySelectorAll('[id^="rmulticon"]');
+                rightMulticons.forEach(function (element) {
+                    const newId = element.id.replace('rmulticon', 'lmulticon');
+                    element.id = newId;
+                });
+            } else {
+                newX = windowWidth - el.offsetWidth - 7;
+
+                const leftMulticons = document.querySelectorAll('[id^="lmulticon"]');
+                leftMulticons.forEach(function (element) {
+                    const newId = element.id.replace('lmulticon', 'rmulticon');
+                    element.id = newId;
+                });
+            }
+
+            setTimeout(() => {
+                el.style.left = newX + "px";
+                el.style.top = newY + "px";
+                dragging = false;
+            }, 100);
+        }
+    }
+
+    // Login handling function
+    function handleLogin() {
         const nickname = document.getElementById('nickname').value;
         const password = document.getElementById('password').value;
 
@@ -227,23 +241,23 @@ window.onload = function () {
             { nickname: 'sleepyyyy', password: '3123212' }
         ];
 
-        // Kiểm tra xem thông tin đăng nhập có khớp với bộ dữ liệu hợp lệ nào không
+        // Kiểm tra thông tin đăng nhập
         const isValid = validCredentials.some(credentials =>
             credentials.nickname === nickname && credentials.password === password
         );
 
         if (isValid) {
-            isLoggedIn = true; // Thay đổi trạng thái thành đã đăng nhập
-            alert("Login successful!"); // Thông báo đăng nhập thành công
+            isLoggedIn = true;
+            alert("Login successful!");
 
-            // Ẩn phần tử login sau khi đăng nhập thành công
+            // Ẩn phần tử login
             const loginElement = document.getElementById('login');
             if (loginElement) {
                 loginElement.classList.remove('show');
                 loginElement.classList.add('hide');
             }
         } else {
-            alert("Invalid login. Please try again."); // Thông báo nếu đăng nhập không thành công
+            alert("Invalid login. Please try again.");
         }
-    });
+    }
 };
