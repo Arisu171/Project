1c514014ed6dcc4ab22047615ee83db8f52ceb3d
BAO035800 <baomoren50@gmail.com>
2024-10-24 18:49:23 +0700

    12342142

--- Home/Transport/Car.js
+++ Home/Transport/Car.js
@@ -12,3 +12,18 @@ function check(clickedLabel) {
     clickedLabel.style.backgroundColor = "#DD8834"; // Đặt màu nền
     clickedLabel.querySelector('.custom-radio').style.color = "white"; // Đặt màu chữ thành trắng
 }
+
+function responsiveInput() {
+    const inputs = document.getElementsByClassName('desktop-input');
+    for (let i = 0; i < inputs.length; i++) {
+        if (window.innerWidth <= 780) {
+            inputs[i].type = 'datetime-local';
+        } else {
+            inputs[i].type = 'date';
+        }
+    }
+}
+
+// Gọi hàm khi tải trang và khi thay đổi kích thước màn hình
+
+
