901f3ac99af961ec85f5ed06bd58973b35ede357
BAO035800 <baomoren50@gmail.com>
2024-10-25 11:38:18 +0700

    13123221

---
+++ Home/PAY/PAY.js
@@ -0,0 +1,12 @@
+function loadPay(file) {
+    // Lưu nội dung hiện tại vào previousContent
+    const dashContentElement = document.getElementById("payment");
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", file, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            dashContentElement.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+}
